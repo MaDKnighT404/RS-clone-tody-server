61bf1c88585901e3dffb56c1f519919d1dbe0ead
George Koloidi <Battleaxe@bk.ru>
2023-02-10 15:46:17 +0100

    fix: create and update

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -79,7 +79,7 @@ export const create = async (req, res) => {
   try {
     const doc = new TodoModel({
       title: req.body.title,
-      text: req.body.text,
+      note: req.body.note,
       user: req.userId,
     });
 
@@ -102,7 +102,7 @@ export const update = async(req, res) => {
       _id: todoId,
     }, {
       title: req.body.title,
-      text: req.body.text,
+      note: req.body.note,
       user: req.userId,
     })
 
