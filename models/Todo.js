1730b52f1d7e60a6bbbdc9027363dfae12cdc9fc
George Koloidi <Battleaxe@bk.ru>
2023-02-13 23:30:23 +0100

    feat: createAt created with timestamp

--- models/Todo.js
+++ models/Todo.js
@@ -1,46 +1,44 @@
 import mongoose from 'mongoose';
 
-const TodoSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    isComplete: {
-      type: Boolean,
-      default: false,
-    },
-    deadlineId: {
-      type: String,
-      default: 'today',
-    },
-    createdAt: {
-      type: Number,
-      default: 1,
-    },
-    deadlineAt: {
-      type: Number,
-      default: 0,
-    },
-    pomodorosNumber: {
-      type: Number,
-      default: 0,
-    },
-    pomodoroTime: {
-      type: Number,
-      default: 0,
-    },
-    completedPomodors: {
-      type: Number,
-      default: 0,
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
+const TodoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
   },
-);
+  isComplete: {
+    type: Boolean,
+    default: false,
+  },
+  deadlineId: {
+    type: String,
+    default: 'today',
+  },
+  createdAt: {
+    type: Number,
+    default: Date.now(),
+  },
+  deadlineAt: {
+    type: Number,
+    default: 0,
+  },
+  pomodorosNumber: {
+    type: Number,
+    default: 0,
+  },
+  pomodoroTime: {
+    type: Number,
+    default: 0,
+  },
+  completedPomodors: {
+    type: Number,
+    default: 0,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
 
 export const TodoModel = mongoose.model('Todo', TodoSchema);
