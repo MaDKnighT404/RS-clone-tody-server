9fdc22febb039364c76e59b0b88d960a55304c09
George Koloidi <Battleaxe@bk.ru>
2023-02-15 11:08:10 +0100

    fix: title todo

--- models/Todo.js
+++ models/Todo.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const TodoSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true,
+    default: ''
   },
   isCompleted: {
     type: Boolean,
